f4ac3c882cb41ed3ffa630c717af8029dbcb8090
Jhoan Narváez <jnarvaez@pcaingenieriacol.onmicrosoft.com>
2024-05-26 18:44:37 -0500

    Routing and customization of the navbar component was created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Router from './Router/router';
 import './App.css'
+import { Layout } from './Pages/Layout/Layout';
+import { Login } from './Pages/Login/Login';
+import { Menu } from './Pages/Menu/Menu';
+import { WritingTest } from './Pages/WritingTest/WritingTest';
+import { ReadingTest } from './Pages/ReadingTest.js/ReadingTest';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Menu" element={<Layout><Menu /></Layout>} />
+          <Route path="/WritingTest" element={<Layout><WritingTest /></Layout>} />
+          <Route path="/ReadingTest" element={<Layout><ReadingTest /></Layout>} />
+
+          {/* Ruta para manejar todas las rutas no definidas */}
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
